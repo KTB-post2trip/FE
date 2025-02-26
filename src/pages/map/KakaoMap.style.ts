f90972ac71527a82378d3efa3dae5e4d87f2e2c5
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-02-26 18:09:08 +0900

    Fix : 사이드 모달 제대로 안 뜨는 에러 해결

--- src/pages/map/KakaoMap.style.ts
+++ src/pages/map/KakaoMap.style.ts
@@ -1,15 +1,34 @@
 import styled from "styled-components";
 
 export const Main = styled.div`
+  position: relative;
   width: 100vw;
   height: 100vh;
-  min-height: 300px;
+  display: flex;
+`;
+
+export const SidebarWrapper = styled.div`
+  position: relative;
+  width: 400px;
+  background-color: pink;
+  box-shadow: 2px 0 5px rgba(0, 0, 0, 0.2);
+  z-index: 10;
 `;
 
 export const MapContainer = styled.div`
-  width: 100%;
+  flex: 1;
   height: 100%;
-  min-height: 300px;
+  background-color: transparent;
   position: relative;
-  z-index: 10;
+  z-index: 1;
+`;
+
+export const Edge = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  width: 5px;
+  cursor: col-resize;
+  background-color: gray;
 `;
